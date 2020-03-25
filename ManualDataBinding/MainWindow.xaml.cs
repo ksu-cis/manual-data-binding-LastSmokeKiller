7ec9f37de8afab46d0b387af78ba8af2e1fed91d
LastSmokeKiller <54453964+LastSmokeKiller@users.noreply.github.com>
2020-03-25 18:17:55 -0500

    Done!

--- ManualDataBinding/MainWindow.xaml.cs
+++ ManualDataBinding/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ManualDataBinding.Data;
 
 namespace ManualDataBinding
 {
@@ -20,9 +21,47 @@ namespace ManualDataBinding
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        Note note = new Note();
+
         public MainWindow()
         {
             InitializeComponent();
+            Editor.Note = note;
+        }
+
+        /// <summary>
+        /// Creates a new not
+        /// </summary>
+        /// <param name="sender">button</param>
+        /// <param name="e">click</param>
+        public void OnNewNote(object sender, RoutedEventArgs e)
+        {
+            note = new Note();
+            Editor.Note = note;
+            
+        }
+
+        /// <summary>
+        /// Clears the notepad
+        /// </summary>
+        /// <param name="sender">button</param>
+        /// <param name="e">click</param>
+        public void OnClearNote(object sender, RoutedEventArgs e)
+        {
+            note.Body = "";
+            
+        }
+
+        /// <summary>
+        /// Lets you know you are simp
+        /// </summary>
+        /// <param name="sender">button</param>
+        /// <param name="e">click</param>
+        public void OnMutateNote(object sender, RoutedEventArgs e)
+        {
+            note.Title = "SIMP!!!";
+            note.Body = "You are a simp...";
         }
     }
 }
